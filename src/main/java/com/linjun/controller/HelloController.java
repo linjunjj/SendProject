7f1bc08b197a5edd15c55c2e18e86956962259d8
linjun <1045735402@qq.com>
2018-03-18 18:38:18 +0800

    sadsad

--- src/main/java/com/linjun/controller/HelloController.java
+++ src/main/java/com/linjun/controller/HelloController.java
@@ -1,22 +1,22 @@
-//package com.linjun.controller;
-//
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.ModelMap;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.ResponseBody;
-//
-//@Controller
-//public class HelloController {
-//    @ResponseBody
-//    @RequestMapping("/hello")
-//    public String hello() {
-//        return "Hello World";
-//    }
-//    @RequestMapping("/")
-//    public String index(ModelMap map) {
-//        // 加入一个属性，用来在模板中读取
-//        map.addAttribute("host", "http://blog.didispace.com");
-//        // return模板文件的名称，对应src/main/resources/templates/index.html
-//        return "index";
-//    }
-//}
+package com.linjun.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+    @ResponseBody
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
+    @RequestMapping("/")
+    public String index(ModelMap map) {
+        // 加入一个属性，用来在模板中读取
+        map.addAttribute("host", "http://blog.didispace.com");
+        // return模板文件的名称，对应src/main/resources/templates/index.html
+        return "index";
+    }
+}
